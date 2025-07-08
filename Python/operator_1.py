c2cb1bda10317cad361fa1fc18129c6c6b9315c1
Jethromwantok <130243517+Jethromwantok@users.noreply.github.com>
2025-07-08 14:19:48 +0100

    Intro py

---
+++ Python/operator_1.py
@@ -0,0 +1,48 @@
+name = "John Doe"
+print(name);
+#Comment
+'''
+This is for 
+Multiple line comments
+i.e triple quotes
+'''
+
+#Choosing Variable names
+
+"""
+1. camel case: e.g firstName, fullName
+2. pascal case: e.g FirstName, FullName
+3. snake case: first_name, full_name
+
+"""
+
+#Data Types
+"""
+int, string, bool, float, array, objects, coplex
+"""
+
+num = 5
+num1 = 5.5
+
+print('Jethro\'s house')
+i1 = 1
+str2 = "i built this program from the ground up"
+bool3 = True
+float4 = 45.67
+array5 = ["Apple", "pineapple" , "watermelon"]
+comp = 7j
+
+print(type(array5))
+print(type(comp))
+print(type(float4))
+print(type(bool3))
+print(type(str2))
+print(type(i1))
+
+"""
+Operators
+1 Arithmetic Operators; +,-,?,*,**,%,//
+2 Assignment Operator; +=, =, -=, *= **=, /=, %=, //=
+3 Comparison operator; <,>,<=,>=,!=,==,===
+4 Logical Operator; 'or', 'and', 
+"""
