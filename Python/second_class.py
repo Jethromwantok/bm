b1e88005e3c048040717e7405dfedfef54cab477
Jethromwantok <130243517+Jethromwantok@users.noreply.github.com>
2025-07-11 15:13:58 +0100

    List types in python

--- Python/second_class.py
+++ Python/second_class.py
@@ -1,7 +1,7 @@
 """
 Python has 4 data collectors: List, set, tuple and dictionary
 List is a data collection that is ordered, changeable and allows duplicates.
-A list can accept any data type of variable e.g Integer, numbers, float, complex and other data collectors
+A list can accept any data type of variable e.g. Integer, numbers, float, complex and other data collectors
 
 list-[]
 Ordered (Has a specific order and can be accessed using the index).
@@ -58,3 +58,32 @@ print(num[3][0])
 students = ["John", "Peter", ["Mary", "Jane", "Lucy"], "Joel", "Paul"]
 
 print(students[2][1])
+"""
+#The ":" is to give a range of printing in a list
+The format is listname[Proposed_start : proposed_stop]
+
+
+
+"""
+
+print(students[:])  # This prints from the beginning to the end
+print(students[:4])  # This prints from the beginning to index four
+print(students[2:])  # This prints from index two to the end
+print(students[2:4])  # This prints from index two to index four
+print(students[0: -1])
+
+# Formatting statements
+print(f"{students[0]} is not my type")  # The 'f' is for formatting and concatenation
+
+"""
+Given the list people = ["Ike","Ibe","Lawal","Yahaya","Ola","Jide"]
+print from Ibe to Ola using negative index
+print from Lawal to the end of the list
+print from the beginning of the list to yahaya
+
+"""
+people = ["Ike", "Ibe", "Lawal", "Yahaya", "Ola", "Jide"]
+
+print(people[-5: -1])
+print(people[2:])
+print(people[:-2])
