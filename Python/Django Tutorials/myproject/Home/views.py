d3e0b289b401a208063ceabd013011b50d351dd4
Jethromwantok <130243517+Jethromwantok@users.noreply.github.com>
2025-10-14 10:40:05 +0100

    Django Apps

---
+++ Python/Django Tutorials/myproject/Home/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+# Pythin views are functions that we use to organize the
+# Content of a webpage
+# Types - Two: Function views and classes based views
+
+def home_view(request):
+    return HttpResponse("<h1 style = 'color: navy'>This is our Home Page</h1>")
